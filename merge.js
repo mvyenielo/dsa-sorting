f5589ea6ff2c1bfef81ac610e8523adc795790a5
Jung-GunSong <jsong781@gmail.com>
2023-11-10 15:41:40 -0500

    completed merge sort

--- merge.js
+++ merge.js
@@ -1,5 +1,56 @@
-function merge() {}
+function merge(left, right) {
+ let output = [];
+ let leftPointer = 0;
+ let rightPointer = 0;
 
-function mergeSort() {}
+
+ while (leftPointer < left.length && rightPointer < right.length){
+
+  if (left[leftPointer] < right[rightPointer]){
+    output.push(left[leftPointer]);
+    leftPointer++;
+  }else {
+    output.push(right[rightPointer]);
+    rightPointer++;
+  }
+
+}
+
+while (leftPointer < left.length){
+  output.push(left[leftPointer]);
+  leftPointer++;
+}
+while (rightPointer < right.length){
+  output.push(right[rightPointer]);
+  rightPointer++;
+}
+
+return output;
+}
+
+function mergeSort(values) {
+
+  if (values.length <= 1){
+    // console.log(`small values are`, values);
+    return values;
+  }
+
+  let middlePointer = Math.floor(values.length/2);
+
+
+  let leftHalf = mergeSort(values.slice(0,middlePointer));
+  //mergeSort([4,20,12]);
+  //mergeSort([4]);
+
+  let rightHalf = mergeSort(values.slice(middlePointer));
+
+  console.log(`left half is`, leftHalf);
+  console.log(`right half is `, rightHalf);
+  console.log(`result of merge is`,merge(leftHalf, rightHalf))
+  return merge(leftHalf, rightHalf);
+
+
+
+}
 
 module.exports = { merge, mergeSort};
