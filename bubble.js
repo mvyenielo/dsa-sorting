cc69352df51fab5e76d06778498b45bbdf06e27a
Megan <mvyenielo58@gmail.com>
2023-11-10 11:09:12 -0800

    finshed bubble sort

--- bubble.js
+++ bubble.js
@@ -1,3 +1,15 @@
-function bubbleSort() {}
+function bubbleSort(values) {
+  for (let i = values.length - 1; i > -1; i--) {
+    for (let j = 0; j < i; j++) {
+     if(values[j] > values[j + 1]) {
+      let temp = values[j];
+      values[j] = values[j + 1];
+      values[j + 1] = temp;
+     }
+    }
+  }
+
+  return values;
+}
 
 module.exports = bubbleSort;
